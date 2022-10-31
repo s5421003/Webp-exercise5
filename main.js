84a42bdccad0b6c2ca9b0f2933213e1c88569593
s5421003 <114901226+s5421003@users.noreply.github.com>
2022-10-31 16:04:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,11 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-
-
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for( let i = 0; i < 5; i++ ) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', pici);
+  newImage.setAttribute('alt', pici.jpg);
+  thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
